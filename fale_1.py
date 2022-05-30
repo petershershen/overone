9440d1d7248405b95c73f472f4d393bc5b162f88
petersherhen <kifa72@mail.ru>
2022-05-30 21:46:40 +0300

    update fale_1.py

--- fale_1.py
+++ fale_1.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
+
 ls = [int(i) for i in input().split()]
 print(ls)
-=======
-ls = [int(i) for i in input().split() if i % 2 ==0]
-
->>>>>>> c5f17b69a498c99b112b4e5c16bdcf3dd44cc9c1
+print("hghg")
